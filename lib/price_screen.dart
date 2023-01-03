30bfc37b63c7b26d7534c0f10cb78ea2221d60fb
jaggadas <jaggadas123@gmail.com>
2023-01-03 21:45:32 +0530

    first commit

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
+import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io'show Platform;
+
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,46 +10,92 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
+  String selectedCurrency = 'USD';
+  DropdownButton<String> getDropdownButton() {
+    List<DropdownMenuItem<String>> dropDownMenuItems = [];
+    setState(() {
+      for (String currency in currenciesList) {
+        var item = DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        );
+        dropDownMenuItems.add(item);
+      }
+    });
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: dropDownMenuItems,
+        onChanged: (value) {
+          setState(() {
+            selectedCurrency = value;
+          });
+        });
+  }
+  CupertinoPicker createRollerMenu() {
+
+  List<Text> rollerMenu = [];
+  for (String currency in currenciesList) {
+    var item = Text(currency);
+    rollerMenu.add(item);
+  }
+
+  return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32,
+      onSelectedItemChanged: (value) {
+        print(value);
+      },
+      children: rollerMenu);
+}
+  Widget getPicker(){
+    if(Platform.isAndroid){
+      return getDropdownButton();
+    }
+    else if(Platform.isIOS){
+      return createRollerMenu();
+    }
+
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('🤑 Coin Ticker'),
+    ),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          child: Card(
+            color: Colors.lightBlueAccent,
+            elevation: 5.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+              child: Text(
+                '1 BTC = ? USD',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
                 ),
               ),
             ),
           ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
+        ),
+        Container(
+          height: 150.0,
+          alignment: Alignment.center,
+          padding: EdgeInsets.only(bottom: 30.0),
+          color: Colors.lightBlue,
+          child:getPicker()
+        ),
+      ],
+    ),
+  );
+}}
